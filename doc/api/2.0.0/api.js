d4fa40fe560f687d27da4363cd9d303f56f0d402
Brian J. Miller <brianmiller@residualselfimage.com>
2017-08-18 13:55:56 -0500

    API doc at 2.0.0

---
+++ doc/api/2.0.0/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Cmi5"
+    ],
+    "modules": [
+        "Cmi5"
+    ],
+    "allModules": [
+        {
+            "displayName": "Cmi5",
+            "name": "Cmi5",
+            "description": "cmi5.js AU runtime library"
+        }
+    ],
+    "elements": []
+} };
+});
